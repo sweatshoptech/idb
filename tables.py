73e32fcf3acb4121919029dfac2dc8a97b3845c1
Vishnu Thiagarajan <aaron.j.m.chang@gmail.com>
2017-03-22 03:00:51 +0000

    adding object tables and get all, closes #16

---
+++ tables.py
@@ -0,0 +1,41 @@
+#!flask/bin/python
+
+"""Grids used in HTML pages"""
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_table import Table, Col
+import models
+
+class CompanyTable(Table):
+    name = Col('Company')
+    website = Col('Website')
+    ownership_type = Col('Ownership')
+    funding = Col('Funding')
+    ceo_id = Col('CEO')
+    size = Col('Size')
+    location = Col('Location')
+
+class SchoolTable(Table):
+    name = Col('School')
+    website = Col('Website')
+    size = Col('Size')
+    location = Col('Location')
+
+class InvestorTable(Table):
+    name = Col('Investor')
+    website = Col('Website')
+    location = Col('Location')
+
+class PersonTable(Table):
+    name = Col('Person')
+    website = Col('Website')
+    title = Col('Title')
+    dob = Col('Date of Birth')
+    location = Col('Location')
+
+table_mappings = {models.Company : CompanyTable, models.Person : PersonTable, models.Investor : InvestorTable, models.School : SchoolTable}
+def get_table_html(model):
+    companies = model.query.all()
+    table = table_mappings[model](companies)
+    return table.__html__()
