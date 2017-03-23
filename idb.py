8348b7bca62b4ec7c790fac09a3315c61c612d33
Vishnu Thiagarajan <thiagarajanvishnu@gmail.com>
2017-03-23 02:19:52 +0000

    templates, css

--- idb.py
+++ idb.py
@@ -7,42 +7,58 @@ import tables
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 @app.route('/index.html', methods=['GET', 'POST'])
 def home_page():
     return render_template('index.html')
 
-
 @app.route('/about')
 @app.route('/about.html')
 def about():
     return render_template('about.html')
-
 
 @app.route('/companies')
 @app.route('/companies.html')
 def companies():
     return render_template('companies.html', companies=tables.get_table_html(models.Company))
 
+@app.route('/company/<int:company_id>')
+def company_template(company_id):
+    company = models.Company.query.get(company_id)
+    return render_template('company_template.html', company=company)
+
+@app.route('/person/<int:person_id>')
+def person_template():
+    return render_template('companies.html', companies=tables.get_table_html(models.Company))
+
+@app.route('/school/<int:school_id>')
+def school_template():
+    return render_template('companies.html', companies=tables.get_table_html(models.Company))
+
+@app.route('/investor/<int:investor_id>')
+def investor_template():
+    return render_template('companies.html', companies=tables.get_table_html(models.Company))
 
 @app.route('/schools')
 @app.route('/schools.html')
 def schools():
     return render_template('schools.html', schools=tables.get_table_html(models.School))
 
-
 @app.route('/investors')
 @app.route('/investors.html')
 def investors():
     return render_template('investors.html', investors=tables.get_table_html(models.Investor))
-
 
 @app.route('/people')
 @app.route('/people.html')
 def people():
     return render_template('people.html', people=tables.get_table_html(models.Person))
 
+@app.route('/implementation')
+@app.route('/implementation.html')
+def report():
+    return render_template('implementation.html')
+
 if __name__ == '__main__':
-    app.run()
+  app.run()
--- tables.py
+++ tables.py
@@ -4,11 +4,11 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_table import Table, Col
+from flask_table import Table, Col, DateCol
 import models
 
-
 class CompanyTable(Table):
+    idnum = Col('ID')
     name = Col('Company')
     website = Col('Website')
     ownership_type = Col('Ownership')
@@ -17,31 +17,49 @@ class CompanyTable(Table):
     size = Col('Size')
     location = Col('Location')
 
+    def get_tr_attrs(self, item):
+        href_text = 'company/{0}'.format(item.idnum)
+        return {'class' : 'clickable-row', 'data-href': href_text}
+
 
 class SchoolTable(Table):
+    idnum = Col('ID')
     name = Col('School')
     website = Col('Website')
     size = Col('Size')
     location = Col('Location')
 
+    def get_tr_attrs(self, item):
+        href_text = 'school/{0}'.format(item.idnum)
+        return {'class' : 'clickable-row', 'data-href': href_text}
+
 
 class InvestorTable(Table):
+    idnum = Col('ID')
     name = Col('Investor')
     website = Col('Website')
-    location = Col('Location')
     funding = Col('Funding')
+    location = Col('Location')
+
+    def get_tr_attrs(self, item):
+        href_text = 'investor/{0}'.format(item.idnum)
+        return {'class' : 'clickable-row', 'data-href': href_text}
 
 
 class PersonTable(Table):
+    idnum = Col('ID')
     name = Col('Person')
     website = Col('Website')
     title = Col('Title')
-    dob = Col('Date of Birth')
+    dob = DateCol('Date of Birth', date_format="medium")
     location = Col('Location')
 
-table_mappings = {models.Company: CompanyTable, models.Person:
-                  PersonTable, models.Investor: InvestorTable, models.School: SchoolTable}
+    def get_tr_attrs(self, item):
+        href_text = 'person/{0}'.format(item.idnum)
+        return {'class' : 'clickable-row', 'data-href': href_text}
 
+
+table_mappings = {models.Company : CompanyTable, models.Person : PersonTable, models.Investor : InvestorTable, models.School : SchoolTable}
 
 def get_table_html(model):
     companies = model.query.all()
