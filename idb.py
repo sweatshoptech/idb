99eb2035bc5f47fc3890668858e645a826bf6a76
Vishnu Thiagarajan <thiagarajanvishnu@gmail.com>
2017-04-20 16:53:03 +0000

    API CORS

--- idb.py
+++ idb.py
@@ -14,12 +14,18 @@ import re
 import csv
 
 app = Flask(__name__)
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    return response
+
 manager = flask_restless.APIManager(app, flask_sqlalchemy_db=models.db)
 manager.create_api(models.Person, methods=['GET'])
 manager.create_api(models.Company, methods=['GET'])
 manager.create_api(models.Investor, methods=['GET'])
 manager.create_api(models.School, methods=['GET'])
-
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
